ea39e443d039fe20cf7054f7804b50694da7d6a4
Maks <maksymhovorun@gmail.com>
2018-10-10 11:23:51 +0300

    minor optimization of Assert class

--- tests/utils/asserts.py
+++ tests/utils/asserts.py
@@ -1,17 +1,13 @@
 class Assert:
 
     @staticmethod
-    def __getPrettyMessage__(shouldPass, message):
-        messageraw = "Test expected to {} but didn't. {}"
-        if shouldPass:
-            return messageraw.format("Pass", message)
-        else:
-            return messageraw.format("Fail", message)
+    def __getPrettyMessage__(resolution, message):
+        return f"Test expected to {resolution} but didn't. {message}"
 
     @staticmethod
     def assert_with_condition(condition, shouldPass, **kwargs):
 
         if shouldPass:
-            assert condition is True, Assert.__getPrettyMessage__(shouldPass, kwargs.get('message', ""))
+            assert condition is True, Assert.__getPrettyMessage__("Pass", kwargs.get('message', ""))
         else:
-            assert condition is False, Assert.__getPrettyMessage__(shouldPass, kwargs.get('message', ""))
+            assert condition is False, Assert.__getPrettyMessage__("Fail", kwargs.get('message', ""))
